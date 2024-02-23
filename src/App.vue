8b4d32953a4e7cf90bc24d455af3b635d36c0414
joaovolnei <joaovolneisilva@gmail.com>
2024-02-23 15:10:14 -0300

    hoje foi produtivo, ta pago

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,17 @@ const contador = ref(0)
 
 <template>
   <div>
-    <button @click="contador++"></button>
-    <button @click="contador--"></button>
+    <button @click="contador++">+</button>
+    <button @click="contador--">-</button>
     <p>Valor do contador: {{ contador }}</p>
   </div>
 </template>
 
 <style scoped>
-
+button {
+background-color: rgb(0, 225, 255);
+color: rgb(255, 0, 0);
+padding: 20px;
+}
 </style>
+
